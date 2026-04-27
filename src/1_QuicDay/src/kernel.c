d05a79b6bb845bf0357329768d4dc5e2594065df
Carlos Mauricio Leal Andersen <carlos.mauricio.leal@outlook.com>
2026-04-27 19:14:28 +0200

    Wrap UserNenu() contents in a while loop

--- src/1_QuicDay/src/kernel.c
+++ src/1_QuicDay/src/kernel.c
@@ -68,7 +68,6 @@ void assignment_2() {
     clear_text(0x0F);
     reset_cursor();
     printf("Hello World");
-    wait_for_user_next_screen();
 }
 
 // assignment 3 (usikker på om jeg har fått denne riktig)
@@ -111,60 +110,75 @@ void assignment_5() {
 }
 
 void userMenu() {
-    int choice = 0;
+    bool user_exit = false;
+    while (!user_exit) {
+        clear_text(0x0F);
+        clear_text(0x0F);
+        int choice = 0;
 
 
-    printf("   ___       _    ___               ___  ___ \n");
-    printf("  / _ \\ _  _(_)__|   \\ __ _ _  _   / _ \\/ __|\n");
-    printf(" | (_) | || | / _| |) / _` | || | | (_) \\__ \\\n");
-    printf("  \\__\\_\\_,_|_\\__|___/\\__,_|\\_, |  \\___/|___/\n");
-    printf("              --QuicDay OS  |__/              \n");
-    printf("\n");
-    printf("\n");
-    printf("Select an option:\n");
-    printf("1. Booting and Printing\n");
-    printf("2. Interrupts\n");
-    printf("3. Memory and PIT\n");    
-    printf("4. Music Player\n");
-    printf("5. Piano\n");
-    printf("6. Exit\n");
+        printf("   ___       _    ___               ___  ___ \n");
+        printf("  / _ \\ _  _(_)__|   \\ __ _ _  _   / _ \\/ __|\n");
+        printf(" | (_) | || | / _| |) / _` | || | | (_) \\__ \\\n");
+        printf("  \\__\\_\\_,_|_\\__|___/\\__,_|\\_, |  \\___/|___/\n");
+        printf("              --QuicDay OS  |__/              \n");
+        printf("\n");
+        printf("\n");
+        printf("Select an option:\n");
+        printf("1. Booting and Printing\n");
+        printf("2. Interrupts\n");
+        printf("3. Memory and PIT\n");    
+        printf("4. Music Player\n");
+        printf("5. Piano\n");
+        printf("6. Exit\n");
 
-    while (1) {
-        if (inb(0x64) & 0x01) {
-            uint8_t scancode = inb(0x60);
-            if (scancode >= 0x02 && scancode <= 0x07) { // 1 til 6
-                choice = scancode - 0x01; // 1 til 6
-                break;
+        while (1) {
+            if (inb(0x64) & 0x01) {
+                uint8_t scancode = inb(0x60);
+                if (scancode >= 0x02 && scancode <= 0x07) { // 1 til 6
+                    choice = scancode - 0x01; // 1 til 6
+                    break;
+                }
             }
         }
-    }
 
-    // needs return functionality
-    switch (choice) {
-        case 1:
-            assignment_2();
-            break;
-        case 2:
-            assignment_3();
-            break;
-        case 3:
-            assignment_4();
-            break;
-        case 4:
-            assignment_5();
-            break;
-        case 5:
-            printf("     QuicDay Piano!     \n");
-            printf("Play by pressing 1-8 on the keypad\n");
-            printf("Press 9 to return to menu\n\n");
-            piano_play_sound_keys();
-            clear_text(0x0F);
-            break;
-        case 6:
-            // Exit message without functionality..
-            printf("byeee!\n");
-            halt_forever();
-            break;
+        // needs return functionality
+        switch (choice) {
+            case 1:
+                assignment_2();
+                break;
+            case 2:
+                assignment_3();
+                break;
+            case 3:
+                assignment_4();
+                break;
+            case 4:
+                assignment_5();
+                break;
+            case 5:
+                printf("     QuicDay Piano!     \n");
+                printf("Play by pressing 1-8 on the keypad\n");
+                printf("Press 9 to return to menu\n\n");
+                piano_play_sound_keys();
+                //clear_text(0x0F);
+                break;
+            case 6:
+                // Exit message without functionality..
+                printf("byeee!\n");
+                user_exit = true;
+                halt_forever();
+                break;
+            default:
+                printf("Invalid choice, please try again.\n");
+                break;
+        }
+        // At the end of each action, clear the screen and loop back to the menu
+        sleep_interrupt(500);
+        wait_for_user_next_screen();
+        sleep_interrupt(500);
+        clear_text(0x0F);
+        clear_text(0x0F);
     }
 }
 
